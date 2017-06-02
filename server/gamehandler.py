816a0fefed5718aeadb4c6b7ecb9071c3b0afd58
Malcolm Vigren <malvi108@student.liu.se>
2017-06-02 22:53:12 +0200

    Added some functions and implementations

--- server/gamehandler.py
+++ server/gamehandler.py
@@ -1,29 +1,61 @@
+import random
+import json
+
+ID_LENGTH = 5
+
+ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 class Player:
 
-    def __init__(self, name):
+    def __init__(self):
         self.score = 0
-        self.name = name
 
 
 class GameState:
 
     def __init__(self):
-        self.players = []
+        self.players = {}
+
+    def add_player(self, name):
+        pass
 
 
-def new_player(request, game_state):
+def handle_request(request, game_state):
+    parsed = json.loads(request)
+    try:
+        action = request['action']
+    except KeyError:
+        raise TypeError('Invalid request! No action key!')
+    if action == 'new':
+        return _new_player(game_state)
+    elif action == 'get':
+        return _get_state(request['id'], game_state)
+    elif action == 'score':
+        return _get_state(request['id'], game_state)
+    elif action == 'push':
+        return _push(request['id'], game_state)
+    else:
+        raise TypeError('Invalid action \'{}\''.format(action))
+
+
+def _new_player(game_state):
     pass
 
 
-def get_score(request, game_state):
+def _get_score(name, game_state):
     pass
 
 
-def get_state(request, game_state):
+def _get_state(name, game_state):
     pass
 
 
-def push(request, game_state):
+def _push(request, game_state):
     pass
 
+
+def _generate_name(players):
+    result = ""
+    for i in range(ID_LENGTH):
+        result += ALPHABET[random.randint(len(ALPHABET) - 1)]
+    return result
