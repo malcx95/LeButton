9cc90a80d69b8fa7e3fc0bb9c5dbdfaa29caa3a5
dawiiee <ledawiiee@gmail.com>
2017-06-02 23:03:47 +0200

    server works with penis

--- server/server.py
+++ server/server.py
@@ -4,15 +4,18 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
     # GET
     def do_GET(self):
+        message="nej"
+        if self.path.endswith("/penis"):
+            message="hest"
         # Send response status code
         self.send_response(200)
 
         # Send headers
         self.send_header('Content-type','text/html')
         self.end_headers()
-
+        
         # Send message back to client
-        message = "Hello world!"
+        # message = "Hello world!" 
         # Write content as utf-8 data
         self.wfile.write(bytes(message, "utf8"))
         return
