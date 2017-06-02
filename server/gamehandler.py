dd5534942acc623575170388db49332386231dc7
Malcolm Vigren <malvi108@student.liu.se>
2017-06-02 22:34:09 +0200

    Added game handler

---
+++ server/gamehandler.py
@@ -0,0 +1,29 @@
+
+class Player:
+
+    def __init__(self, name):
+        self.score = 0
+        self.name = name
+
+
+class GameState:
+
+    def __init__(self):
+        self.players = []
+
+
+def new_player(request):
+    pass
+
+
+def get_score(request):
+    pass
+
+
+def get_state(request):
+    pass
+
+
+def push(request):
+    pass
+
