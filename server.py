d418600412980d84bec1c71f8ec07388a3c7da0d
dawiiee <ledawiiee@gmail.com>
2017-06-02 22:16:48 +0200

    penis

--- server.py
+++ server.py
@@ -1,10 +1,99 @@
-import http.server
 
-PORT = 8000
+from http.server import BaseHTTPRequestHandler, HTTPServer
+ 
+# HTTPRequestHandler class
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+ 
+  # GET
+def do_GET(self):
+        # Send response status code
+        self.send_response(200)
+ 
+        # Send headers
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+ 
+        # Send message back to client
+        message = "Hello world!"
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
+        return
+ 
+def run():
+	print('starting server...')
+ 
+  # Server settings
+  # Choose port 8080, for port 80, which is normally used for a http server, you need root access
+m http.server import BaseHTTPRequestHandler, HTTPServer
+ 
+# HTTPRequestHandler class
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+ 
+  # GET
+  def do_GET(self):
+        # Send response status code
+#!/usr/bin/env python
+ 
+from http.server import BaseHTTPRequestHandler, HTTPServer
+ 
+# HTTPRequestHandler class
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+ 
+  # GET
+  def do_GET(self):
+        # Send response status code
+        self.send_response(200)
+ 
+        # Send headers
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+ 
+        # Send message back to client
+        message = "Hello world!"
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
+        return
+ 
+def run():
+	print('starting server...')
 
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-
-httpd = SocketServer.TCPServer(("", PORT), Handler)
-
-print ("serving at port", PORT)
+	# Server settings
+	# Choose port 8080, for port 80, which is normally used for a http server, you need root access
+	server_address = ('127.0.0.1', 8081)
+	httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+	print('running server...')
 httpd.serve_forever()
+ 
+ 
+run()        self.send_response(200)
+ 
+        # Send headers
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+ 
+        # Send message back to client
+        message = "Hello world!"
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
+        return
+ 
+def run():
+	print('starting server...')
+ 
+    	# Server settings
+    	# Choose port 8080, for port 80, which is normally used for a http server, you need root accesserver_address = ('127.0.0.1', 8081)
+   	 httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+    print('running server...')
+    httpd.serve_forever()
+ 
+ 
+run()
+  server_address = ('127.0.0.1', 8081)
+  httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+  print('running server...')
+  httpd.serve_forever()
+ 
+ 
+run()
+
+
